4a3140954b44ac5054e3af1334d7265136f4fbe3
souhail <souhalshiv@gmail.com>
2024-05-06 21:21:58 +0100

    remove the threads because it's not safe when working with scenes

--- main.cpp
+++ main.cpp
@@ -3,48 +3,40 @@
 #include <SDL2/SDL_image.h>
 #include "game.hpp"
 using namespace std;
-int UpdateAndRender(void *data)
+
+int WinMain()
 {
+    srand(time(NULL));
+
+    game game("game with no name");
     Uint32 framesStart;
-    double deltaTime = 0.0f;
     double maxDeltaTime = 0.05f;
+    double deltaTime = 0.0f;
     Uint32 lastFrameTime = SDL_GetTicks();
-    game *mygame = (game *)data;
-    while (mygame->getRunning())
+
+    while (game.getRunning())
     {
         framesStart = SDL_GetTicks();
         int frameTime;
-        // mygame->handleEvents();
-        mygame->update(deltaTime);
-        mygame->render();
-        frameTime = SDL_GetTicks() - framesStart;
+
+        game.handleEvents();
+
         Uint32 currentFrameTime = SDL_GetTicks();
         deltaTime = (currentFrameTime - lastFrameTime) / 1000.0f;
         if (deltaTime > maxDeltaTime)
             deltaTime = maxDeltaTime;
         lastFrameTime = currentFrameTime;
-        // printf("deltaTime:%f\n", deltaTime);
+
+        game.update(deltaTime);
+        game.render();
+
+        frameTime = SDL_GetTicks() - framesStart;
 
         if (FRAMEDELAY > frameTime)
         {
             SDL_Delay(FRAMEDELAY - frameTime);
         }
     }
-    return 0;
-}
-int WinMain()
-{
-    srand(time(NULL));
-    game game("game with no name");
-    SDL_Thread *updateThread = SDL_CreateThread(UpdateAndRender, "UpdateAndRender", (void *)&game);
-
-    while (game.getRunning())
-    {
-        game.handleEvents();
-
-        SDL_Delay(FRAMEDELAY);
-    }
-    SDL_WaitThread(updateThread, NULL);
 
     SDL_Quit();
     return 0;
