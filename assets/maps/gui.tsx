c3ef84b988c2439ca9c501606c8965e625367421
souhail <souhalshiv@gmail.com>
2024-05-10 01:00:34 +0100

    assets

---
+++ assets/maps/gui.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="gui" tilewidth="44" tileheight="44" tilecount="156" columns="12">
+ <image source="../gfx/gui.png" width="538" height="592"/>
+</tileset>
---
+++ assets/maps/tileStageForest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileStageForest" tilewidth="16" tileheight="16" tilecount="54" columns="6">
+ <image source="../gfx/tileStageForest.png" width="96" height="144"/>
+</tileset>
